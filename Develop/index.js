125828646323720cb34850b84b3e3b43539f09e5
dlwortmann <dannywortmann@gmail.com>
2025-02-08 12:42:40 -0600

    moved license functions and markdown structure to generateMarkdown.js file

--- Develop/index.js
+++ Develop/index.js
@@ -48,46 +48,21 @@ const questions = [
             name: "email"
         }
     ])
+    .then ((questions) => {
+        writeToFile("sample-README.md, questions")
+    })
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    var inputs = "";
-    inputs += `${data.name}'s README\n\n`;
-    inputs += ` # ${data.title}\n\n`;
-    inputs += `${generateLicense(data.license)}\n\n`;
-    inputs += ' # Table of Contents\n\n';
-    inputs += `* [Description](#description)\n * [Installation](#installation)\n * [Usage](#usage)\n * [Contributing](#contribute)\n * [Tests](#test)\n * [License](#license)\n * [Questions](#questions)\n\n`;
-    inputs += `## Description\n${data.description}\n\n`;
-    inputs += `## Installation\n${data.installation}\n\n`;
-    inputs += `## Usage Information\n${data.usage}\n\n`;
-    inputs += `## Contribution Guidelines\n${data.contribute}\n\n`;
-    inputs += `## Test Instructions\n${data.test}\n\n`;
-    inputs += `## License\nThis application is covered under the ${data.license}.\n\n`;
-    inputs += `## Questions?\nFollow the link to my Github account to see additional respositories or click my email address to send me a message.\n\n`;
-    inputs += `[Link to Github](http://gitbuh.com/${data.github})\n\n`;
-    inputs += `<a href="mailto:${data.email}">${data.email}</a>\n\n`;
-
-    fs.writeFile(fileName, inputs, (err) => 
+function writeToFile(fileName, questions) {
+    fs.writeFile(fileName, questions, (err) => 
         err ? console.error(err) : console.log("Sucessful data collection."));
 }
 
-//need function to generate licensing badges
-function generateLicense(license) {
-    if(license === "MIT") {
-        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-    } else if(license === "Apache 2.0 License") {
-        return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;    
-    } else if(license === "BSD 3-Clause License") {
-        return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;    
-    } else if(license === "Mozilla Public License 2.0") {
-        return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;    
-    } else if(license === "The Unlicense") {
-        return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;    
-    }  
-
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if(license === "MIT") {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    } else if(license === "Apache 2.0 License") {
+        return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;    
+    } else if(license === "BSD 3-Clause License") {
+        return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;    
+    } else if(license === "Mozilla Public License 2.0") {
+        return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;    
+    } else if(license === "The Unlicense") {
+        return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;    
+    } else {
+      return "";
+    } 
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === "MIT") {
+    return ``;
+} else if(license === "Apache 2.0 License") {
+    return ``;    
+} else if(license === "BSD 3-Clause License") {
+    return ``;    
+} else if(license === "Mozilla Public License 2.0") {
+    return ``;    
+} else if(license === "The Unlicense") {
+    return ``;    
+} else {
+  return "";
+} 
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,9 +41,21 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  var inputs = "";
+  inputs += `${data.name}'s README\n\n`;
+  inputs += ` # ${data.title}\n\n`;
+  inputs += `${renderLicenseBadge(data.license)}\n\n`;
+  inputs += ' # Table of Contents\n\n';
+  inputs += `* [Description](#description)\n * [Installation](#installation)\n * [Usage](#usage)\n * [Contributing](#contribute)\n * [Tests](#test)\n * [License](#license)\n * [Questions](#questions)\n\n`;
+  inputs += `## Description\n${data.description}\n\n`;
+  inputs += `## Installation\n${data.installation}\n\n`;
+  inputs += `## Usage Information\n${data.usage}\n\n`;
+  inputs += `## Contribution Guidelines\n${data.contribute}\n\n`;
+  inputs += `## Test Instructions\n${data.test}\n\n`;
+  inputs += `## License\nThis application is covered under the ${data.license}.\n\n`;
+  inputs += `## Questions?\nFollow the link to my Github account to see additional respositories or click my email address to send me a message.\n\n`;
+  inputs += `[Link to Github](http://gitbuh.com/${data.github})\n\n`;
+  inputs += `<a href="mailto:${data.email}">${data.email}</a>\n\n`;
 }
 
 export default generateMarkdown;
